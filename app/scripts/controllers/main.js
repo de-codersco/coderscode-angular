fdf2eca69227c9f8140e20551ed9e023e1ae75bc
de-codersco <de.codersco@gmail.com>
2015-04-07 02:18:01 +0200

    first draft jsonp

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the coderscodeAngularApp
  */
 angular.module('coderscodeAngularApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    var url = 'http://localhost:9000/api/user?callback=JSON_CALLBACK';
+    $http.jsonp(url)
+      .success(function(data){
+        $scope.user = data;
+      });
   });
